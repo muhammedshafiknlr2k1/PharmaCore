ffb1ce8bfeb5d3af6b9c6f1c6842ca899de6037c
SHAFI S <muhammedshafiknlr65@gmail.com>
2026-02-19 07:59:23 +0530

    pharmacy model created

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.core.database import engine, Base
+from app.modules.pharmacy.models import Pharmacy
 
 
 app = FastAPI(title='PharmaCore')
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get('/')
---
+++ app/modules/Pharmacy/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+
+class Pharmacy(Base):
+    __tablename__ = 'pharmacies'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    email: Mapped[str] = mapped_column(String(50),unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    address: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    is_active: Mapped[Boolean] = mapped_column(Boolean, default=True)
